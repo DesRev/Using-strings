e2657b468677736ee800b104345a5dba33718d70
DesRev <130482418+DesRev@users.noreply.github.com>
2023-04-11 12:44:46 -0400

    Add files via upload

---
+++ string_project.py
@@ -0,0 +1,10 @@
+fruit = input("lemons are the best ")
+fruit2 = input("limes are the best ")
+if fruit == fruit2:
+    print("The strings are equal.")
+else:
+    print("The strings are not equal.")
+if fruit != fruit2:           #Here I used the != operator after I used the == operator above
+    print("The strings are not equal.")
+else:
+    print("The strings are equal.")
